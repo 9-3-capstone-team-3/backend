83c72941138844659cafa6f2869862ae35eaa931
SangUn Park <Sangunpark1@gmail.com>
2023-08-23 10:53:51 -0400

    debugging query for User table

--- queries/user.js
+++ queries/user.js
@@ -2,7 +2,7 @@ const db = require("../db/dbConfig");
 
 const getAllUsers = async () => {
   try {
-    const result = await db.any("SELECT * FROM user");
+    const result = await db.any("SELECT * FROM users");
     return { result };
   } catch (error) {
     return { error };
@@ -11,7 +11,7 @@ const getAllUsers = async () => {
 
 const getUser = async (id) => {
   try {
-    const result = await db.one(`SELECT * FROM user where id=${id}`);
+    const result = await db.one(`SELECT * FROM users where id=${id}`);
     return { result };
   } catch (error) {
     return { error };
@@ -20,7 +20,7 @@ const getUser = async (id) => {
  
 const createUser = async (user) => {
     try {
-        const result = await db.one(`INSERT INTO user(username, email, firstname, lastname, password, level_id) 
+        const result = await db.one(`INSERT INTO users(username, email, firstname, lastname, password, level_id) 
         VALUES($1, $2, $3, $4, $5, $6) 
         RETURNING *;`,
         [user.username, user.email, user.firstname, user.lastname, user.password, user.level_id]
@@ -34,7 +34,7 @@ const createUser = async (user) => {
 const deleteUser = async (id) => {
     try {
       const result = await db.one(
-        "DELETE FROM user WHERE id=$1 RETURNING *",
+        "DELETE FROM users WHERE id=$1 RETURNING *",
         id
       );
       return { result };
@@ -46,7 +46,7 @@ const deleteUser = async (id) => {
   const updateUser = async (id, review) => {
     try {
       const result = await db.one(
-        `UPDATE user SET username=$1, email=$2, firstname=$3, lastname=$4, password=$5, level_id=$6 WHERE id=$7 RETURNING *`,
+        `UPDATE users SET username=$1, email=$2, firstname=$3, lastname=$4, password=$5, level_id=$6 WHERE id=$7 RETURNING *`,
         [
           user.username,
           user.email,
