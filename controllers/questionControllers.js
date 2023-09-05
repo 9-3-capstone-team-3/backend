156272ba3bf7e7a06ac73aabd1fa66ad7f311070
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-09-05 12:07:02 -0400

    controller updates

---
+++ controllers/questionControllers.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const question = express.Router();
+
+const validateQueston = require("./validations/validateQuestion");
+const {
+    getAllQuestions,
+    getQuestion,
+} = require("../queries/question");
+
+question.get("/", async (req, res) => {
+    const { error, result} = await getAllQuestions();
+    if (error) {
+        res.status.apply(500).json({ error: "server error"});
+    } else {
+        res.status(200).json(result);
+    }
+});
+
+question.get("/:id", async (req, res) => {
+    const { error, result} = await getQuestion(id);
+    if (error?.code === 0) {
+        res.status(404).json({ error: "question not found"});
+    } else if (error) {
+        res.status(500).json({ error: "server error"});
+    } else {
+        res.status(200).json(result)
+    }
+});
+
+module.exports = question;
+
