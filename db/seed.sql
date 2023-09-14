cf9aab3e323c71f22b862a92a9c04bdf722fc2f5
Karimah Reavis <Karimah586@gmail.com>
2023-09-13 20:13:58 -0400

    fixed bugs and rearranged seed data as needed

--- db/seed.sql
+++ db/seed.sql
@@ -17,11 +17,10 @@ INSERT INTO users (username, email, firstname, lastname, password, level_id, tot
 
 -- Insert quiz (set of questions)
 INSERT INTO quiz(level_id, name, video_url) VALUES 
-(1, 'Initial landing page questions', 'https://youtu.be/hfsP3lXoSMc'),
-(2, 'Git Basics', 'https://youtu.be/GgUIwRtLvrw'),
-(3, 'Setting up Git', 'https://youtu.be/FCBVWCulNgk'),
-(4, 'Initialize Git', 'https://youtu.be/3QHmKcs2MVY'),
-(5, 'Git Commands Branches and Merges', NULL);
+(1, 'Git Basics', 'https://www.youtube.com/embed/hfsP3lXoSMc?si=XezAZW4ajyguURFS'),
+(2, 'Setting up Git', 'https://youtu.be/GgUIwRtLvrw'),
+(3, 'Initialize Git', 'https://youtu.be/FCBVWCulNgk'),
+(4, 'Git Commands Branches and Merges', 'https://youtu.be/3QHmKcs2MVY');
 
 
 -- Insert question types
@@ -30,19 +29,19 @@ INSERT INTO prompt_type(name, points) VALUES
 
 -- Insert questions (without answer_id)
 INSERT INTO question (prompt, quiz_id, prompt_type_id) VALUES
-('What is git?', 2, 1),
-('Where do we get git from?', 2, 1),
-('Where do we use git commands?', 2, 1),
-('Git tracks every version of your code, while gitHub…', 2, 1),
-('Why should we learn how to use git?', 2, 1),
-('Write the git configuration command to set up Git with Github.',3,2),
-('Write the git configuration command that does not require a password.',3,2),
-('We can initialize git on a project that already exists on a local machine',4,3),
-('What is the command to initialize git into an already existing file?',4,2),
-('After you create a repository on gitHub, you should go inside your project terminal and type "git clone http or ssh key"',4,3),
-('Write the command to merge a branch to the main branch', 5, 2),
-('Write the short command to create a branch and switch to a branch',5,2),
-('When you git commit, you must git push to send your commits to the remote GitHub',5,3);
+('What is git?', 1, 1),
+('Where do we get git from?', 1, 1),
+('Where do we use git commands?', 1, 1),
+('Git tracks every version of your code, while gitHub…', 1, 1),
+('Why should we learn how to use git?', 1, 1),
+('Write the git configuration command to set up Git with Github.',2,2),
+('Write the git configuration command that does not require a password.',2,2),
+('We can initialize git on a project that already exists on a local machine',2,3),
+('What is the command to initialize git into an already existing file?',3,2),
+('After you create a repository on gitHub, you should go inside your project terminal and type "git clone http or ssh key"',3,3),
+('Write the command to merge a branch to the main branch', 4, 2),
+('Write the short command to create a branch and switch to a branch',4,2),
+('When you git commit, you must git push to send your commits to the remote GitHub',4,3);
 --RETURNING question_id;
 
 INSERT INTO intro_question (prompt_type_id, prompt) VALUES 
