9f1538c926bbba9637fb0f39a89b73f4532e341e
SangUn Park <Sangunpark1@gmail.com>
2023-09-20 23:19:29 -0400

    removed intro question table, moved it to question table to initialize the dashboard

--- db/schema.sql
+++ db/schema.sql
@@ -61,13 +61,6 @@ CREATE TABLE quiz (
 
 
 
--- Create the intro_question table
-CREATE TABLE intro_question (
-    id SERIAL PRIMARY KEY,
-    created_date TIMESTAMP DEFAULT current_timestamp,
-    prompt_type_id INT,
-    prompt VARCHAR(255) NOT NULL
-);
 
 -- Create the users table without foreign keys
 CREATE TABLE users (
