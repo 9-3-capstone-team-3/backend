5fa462c94ad93dadf877772e39935b7ff4e5f5c9
SangUn Park <Sangunpark1@gmail.com>
2023-10-03 10:13:37 -0400

    changing ENV

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,22 +1,22 @@
 const pgp = require("pg-promise")();
 require("dotenv").config()
 
-const connectionString = process.env.ELEPHANTSQL_URL;
+// const connectionString = process.env.ELEPHANTSQL_URL;
 
-const db = pgp({
-    connectionString: connectionString,
-    ssl: {
-        rejectUnauthorized: false  // Used for ElephantSQL as it requires SSL
-    }
-});
+// const db = pgp({
+//     connectionString: connectionString,
+//     ssl: {
+//         rejectUnauthorized: false  // Used for ElephantSQL as it requires SSL
+//     }
+// });
 
-// const cn = {
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-//     user: process.env.PG_USER // removed an extra space on this line - MR
-// };
+const cn = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER // removed an extra space on this line - MR
+};
 
-// const db = pgp(cn);
+const db = pgp(cn);
 
 module.exports = db;
